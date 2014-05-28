c1d8fc5eb4c7a92847d79646bc8ee78948004118
Miljan Aleksic <miljan@aleksic.es>
2014-05-28 21:44:07 +0200

    Upload comp first draft

---
+++ src/js/upload.js
@@ -0,0 +1,39 @@
+;(function ($, ZX, window, document, undefined) {
+    "use strict";
+
+    var instance_id = 0,
+        cache = {};
+    
+    ZX.component('upload', {
+
+        defaults: {
+            root: '', // relative path to the root folder
+            extensions: '', // array or comma separated values of allowed extensions
+            storage: 'local',
+            storage_params: {},
+            max_file_size: '',
+            resize: {}
+        },
+
+        init: function() {
+            var $this = this;
+
+            // set the resources list
+
+        }
+
+    });
+
+    // init code
+    $(document).on('uk-domready', function(e) {
+
+        $('.zx-manager [data-zx-manager-upload]').each(function() {
+            var manager = $(this);
+
+            if (!manager.data('uploadManager')) {
+                var obj = ZX.uploadManager(manager, $.UIkit.Utils.options(manager.attr('data-zx-manager-upload')));
+            }
+        });
+    });
+
+})(jQuery, jQuery.zlux, window, document);
