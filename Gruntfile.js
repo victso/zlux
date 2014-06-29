252a9d4fc50ebd0f72b489e88889a2e149e68bc3
Miljan Aleksic <miljan@aleksic.es>
2014-06-29 20:25:21 +0200

    Transit Addon for CSS3 transitions and transformations

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,8 @@ module.exports = function(grunt) {
 					{ 'dist/js/zlux.min.js': ['dist/js/zlux.js'] },
 					{ 'dist/js/addons/datatables.min.js': ['dist/js/addons/datatables.js'] },
 					{ 'dist/js/addons/nestable.min.js': ['dist/js/addons/nestable.js'] },
-					{ 'dist/js/addons/notify.min.js': ['dist/js/addons/notify.js'] }
+					{ 'dist/js/addons/notify.min.js': ['dist/js/addons/notify.js'] },
+					{ 'dist/js/addons/animate.min.js': ['dist/js/addons/animate.js'] }
 				]
 			}
 		},
