90b4c30400554182e66d7c7894f4f3b9db3f9d8a
Miljan Aleksic <miljan@aleksic.es>
2015-08-08 20:02:54 +0200

    update dist

--- dist/js/components/files-manager.js
+++ dist/js/components/files-manager.js
@@ -3089,6 +3089,20 @@
 
 	        },
 
+	        computed: {
+
+	            notice: function() {
+	                return this.notices.length ? this.notices.join('\n') : false;
+	            },
+
+	            selected: function () {
+	                return this.resources.filter(function (resource) {
+	                    return resource.selected;
+	                });
+	            }
+
+	        },
+
 	        created: function() {
 
 	            this.$watch('filter', function(value, oldValue) {
@@ -3127,25 +3141,7 @@
 
 	        },
 
-	        computed: {
-
-	            notice: function() {
-	                return this.notices.length ? this.notices.join('\n') : false;
-	            },
-
-	            selected: function () {
-	                return this.resources.filter(function (resource) {
-	                    return resource.selected;
-	                });
-	            }
-
-	        },
-
 	        methods: {
-
-	            changeView: function(view) {
-	                this.currentView = view;
-	            },
 
 	            changePage: function(page) {
 	                this.fetch(null, page);
@@ -3157,22 +3153,42 @@
 
 	            reload: function(e) {
 	                if (e) e.preventDefault();
-
 	                this.cache = {};
 	                this.fetch(this.location, this.currentPage);
 	            },
 
 	            search: function(e) {
 	                if (e) e.preventDefault();
-
 	                this.fetch();
-	                // this.searching = false;
 	            },
 
 	            clearSearch: function() {
 	                this.$set('filter', '');
 	                this.fetch();
-	                // this.searching = false;
+	            },
+
+	            setPageData: function(data) {
+	                this.$set('location', data.location);
+	                this.$set('currentPage', data.page);
+	                this.$set('resources', data.resources);
+	                this.$set('count', data.count);
+	                this.$set('total', data.total);
+
+	                this.cache[data.location + data.page] = data;
+	            },
+
+	            createDir: function() {
+
+	                var path = helper.cleanPath(this.location + '/' + prompt());
+
+	                this.$http.get(this.routeMap + '/createDir', {path: path}).done(function(response) {
+
+	                    this.reload();
+
+	                }).always(function(response) {
+	                    this.riseWarnings(response);
+	                });
+
 	            },
 
 	            deleteSelected: function() {
@@ -3183,12 +3199,10 @@
 
 	                this.$http.get(this.routeMap + '/deleteResources', {resources: resources}).done(function(response) {
 
-	                    this.reload()
+	                    this.reload();
 
 	                }).always(function(response) {
-
-	                    this.riseWarnings(response.errors, response.notices);
-
+	                    this.riseWarnings(response);
 	                });
 
 	            },
@@ -3198,14 +3212,7 @@
 	                page = page || 1;
 
 	                if (this.cache[location + page]) {
-	                    var cached = this.cache[location + page];
-
-	                    this.$set('location', cached.location);
-	                    this.$set('currentPage', cached.page);
-	                    this.$set('resources', cached.resources);
-	                    this.$set('count', cached.count);
-	                    this.$set('total', cached.total);
-	                    return;
+	                    return this.setPageData(this.cache[location + page])
 	                }
 
 	                var params = _.merge({
@@ -3219,37 +3226,28 @@
 
 	                this.$http.get(this.routeMap + '/fetchResources', params).done(function(response) {
 
-	                    this.$set('location', response.location);
-	                    this.$set('currentPage', response.page);
-	                    this.$set('resources', response.resources);
-	                    this.$set('count', response.count);
-	                    this.$set('total', response.total);
-
-	                    this.cache[response.location + this.currentPage] = response;
+	                    this.setPageData(response);
 
 	                    // execute callback
 	                    if (_.isFunction(this.onLoadPage)) {
 	                        this.onLoadPage();
 	                    }
 
-	                }).fail(function(response) {
-
-	                    this.riseWarnings(response.errors, response.notices);
-
-	                }).always(function() {
+	                }).always(function(response) {
+	                    this.riseWarnings(response);
 	                    this.$set('fetching', false);
 	                });
 
 	            },
 
-	            riseWarnings: function (errors, notices) {
+	            riseWarnings: function (response) {
 
-	                if (errors.length) {
-	                    UI.notify(errors.join('\n'), {pos: 'top-right', status: 'danger'});
+	                if (response.errors.length) {
+	                    UI.notify(response.errors.join('\n'), {pos: 'top-right', status: 'danger'});
 	                }
 
-	                if (notices.length) {
-	                    UI.notify(notices.join('\n'), {pos: 'top-right', status: 'warning'});
+	                if (response.notices.length) {
+	                    UI.notify(response.notices.join('\n'), {pos: 'top-right', status: 'warning'});
 	                }
 
 	            }
@@ -3624,7 +3622,7 @@
 /* 98 */
 /***/ function(module, exports) {
 
-	module.exports = "<div class=\"zx-files-manager\">\n\n        <div v-if=\"fetching && !resources.length\" class=\"uk-text-center\">\n            <i class=\"uk-icon-spinner uk-icon-spin uk-icon-small\"></i>\n        </div>\n\n        <!-- main nav -->\n        <nav class=\"uk-navbar\" v-el=\"nav\">\n\n            <form class=\"uk-form uk-margin-remove uk-display-inline-block uk-width-8-10\" v-on=\"submit: search\">\n\n                <div class=\"uk-form-icon uk-width-1-1\">\n                    <i v-class=\"filter ? 'uk-icon-times' : 'uk-icon-search'\" v-on=\"click: clearSearch\"></i>\n                    <input v-model=\"filter\" class=\"uk-form-blank uk-width-1-1\" debounce=\"500\" type=\"search\">\n                </div>\n\n            </form>\n\n            <div class=\"uk-navbar-content uk-navbar-flip\">\n                <a href=\"\" v-on=\"click: reload\" title=\"{{ 'Reload' | trans }}\">\n                    <i class=\"uk-icon-refresh uk-icon-hover\"></i>\n                </a>\n            </div>\n\n        </nav>\n\n        <!-- buttons -->\n        <div class=\"uk-margin\">\n            <span class=\"uk-button uk-button-small uk-button-primary uk-form-file\">{{ 'Upload' | trans }}<input type=\"file\"></span>\n            <button type=\"button\" v-on=\"click: addFolder\" class=\"uk-button uk-button-small\">{{ 'Add Folder' | trans }}</button>\n            <button v-if=\"selected.length\" type=\"button\" v-on=\"click: deleteSelected\" class=\"uk-button uk-button-small uk-button-danger\">{{ 'Delete' | trans }}</button>\n        </div>\n\n        <!-- breadcrumb -->\n        <breadcrumb location=\"{{ location }}\" go-to=\"{{ goTo }}\"></breadcrumb>\n\n        <!-- resources -->\n        <div class=\"uk-overflow-container\">\n            <ul class=\"uk-grid uk-grid-width-small-1-2 uk-grid-width-medium-1-3 uk-grid-width-xlarge-1-4\" data-uk-grid-margin data-uk-grid-match=\"{target:'.uk-panel'}\">\n                <component is=\"resource\" v-repeat=\"resources\"></component>\n            </ul>\n        </div>\n\n        <!-- drop files -->\n        <div class=\"uk-placeholder uk-text-center uk-margin-bottom-remove\">\n            <i class=\"uk-icon-cloud-upload\"></i> {{ 'Drop files here' | trans }}\n        </div>\n\n        <div class=\"uk-progress uk-hidden\">\n            <div class=\"uk-progress-bar\" style=\"width: 0%;\"></div>\n        </div>\n\n        <!-- pagination -->\n        <pagination v-if=\"total > itemsPerPage\" items=\"{{ total }}\" current-page=\"{{@ currentPage }}\" items-on-page=\"{{ itemsPerPage }}\" on-select-page=\"{{ changePage }}\"></pagination>\n\n    </div>";
+	module.exports = "<div class=\"zx-files-manager\">\n\n        <div v-if=\"fetching && !resources.length\" class=\"uk-text-center\">\n            <i class=\"uk-icon-spinner uk-icon-spin uk-icon-small\"></i>\n        </div>\n\n        <!-- main nav -->\n        <nav class=\"uk-navbar\" v-el=\"nav\">\n\n            <form class=\"uk-form uk-margin-remove uk-display-inline-block uk-width-1-1\" v-on=\"submit: search\">\n\n                <div class=\"uk-form-icon uk-width-1-1\">\n                    <i v-class=\"filter ? 'uk-icon-times' : 'uk-icon-search'\" v-on=\"click: clearSearch\"></i>\n                    <input v-model=\"filter\" class=\"uk-form-blank uk-width-1-1\" debounce=\"500\" type=\"search\">\n                </div>\n\n            </form>\n\n        </nav>\n\n        <!-- buttons -->\n        <div class=\"uk-margin\">\n            <span class=\"uk-button uk-button-small uk-button-primary uk-form-file\">{{ 'Upload' | trans }}<input type=\"file\"></span>\n            <button type=\"button\" v-on=\"click: createDir\" class=\"uk-button uk-button-small\">{{ 'Add Folder' | trans }}</button>\n            <button type=\"button\" v-on=\"click: reload\" class=\"uk-button uk-button-small\">{{ 'Reload' | trans }}</button>\n            <button v-if=\"selected.length\" type=\"button\" v-on=\"click: deleteSelected\" class=\"uk-button uk-button-small uk-button-danger\">{{ 'Delete' | trans }}</button>\n        </div>\n\n        <!-- breadcrumb -->\n        <breadcrumb location=\"{{ location }}\" go-to=\"{{ goTo }}\"></breadcrumb>\n\n        <!-- resources -->\n        <div class=\"uk-overflow-container\">\n            <ul class=\"uk-grid uk-grid-width-small-1-2 uk-grid-width-medium-1-3\" data-uk-grid-margin data-uk-grid-match=\"{target:'.uk-panel'}\">\n                <component is=\"resource\" v-repeat=\"resources\"></component>\n            </ul>\n        </div>\n\n        <!-- drop files -->\n        <div class=\"uk-placeholder uk-text-center uk-margin-bottom-remove\">\n            <i class=\"uk-icon-cloud-upload\"></i> {{ 'Drop files here' | trans }}\n        </div>\n\n        <div class=\"uk-progress uk-hidden\">\n            <div class=\"uk-progress-bar\" style=\"width: 0%;\"></div>\n        </div>\n\n        <!-- pagination -->\n        <pagination v-if=\"total > itemsPerPage\" items=\"{{ total }}\" current-page=\"{{@ currentPage }}\" items-on-page=\"{{ itemsPerPage }}\" on-select-page=\"{{ changePage }}\"></pagination>\n\n    </div>";
 
 /***/ }
 /******/ ]);
